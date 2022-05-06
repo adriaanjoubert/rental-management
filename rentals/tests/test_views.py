811f991096479efbd36b6b74deffed5485f5dc50
Adriaan Joubert <45142747+adriaanjoubert@users.noreply.github.com>
2022-05-06 22:25:30 +0100

    Implement view & update test

--- rentals/tests/test_views.py
+++ rentals/tests/test_views.py
@@ -12,22 +12,57 @@ class ReservationListViewTest(TestCase):
 
     def setUp(self) -> None:
         self.test_url = reverse('rentals:list')
-        rental = mommy.make(Rental, name='rental-1')
-        mommy.make(Reservation, rental=rental, checkin=datetime.date(2022, 1, 1), checkout=datetime.date(2022, 1, 13))
+        rental_1 = mommy.make(Rental, name='rental-1')
+        mommy.make(
+            Reservation,
+            id=1,
+            rental=rental_1,
+            checkin=datetime.date(2022, 1, 1),
+            checkout=datetime.date(2022, 1, 13),
+        )
+        mommy.make(
+            Reservation,
+            id=2,
+            rental=rental_1,
+            checkin=datetime.date(2022, 1, 20),
+            checkout=datetime.date(2022, 2, 10),
+        )
+        mommy.make(
+            Reservation,
+            id=3,
+            rental=rental_1,
+            checkin=datetime.date(2022, 2, 20),
+            checkout=datetime.date(2022, 3, 10),
+        )
+        rental_2 = mommy.make(Rental, name='rental-2')
+        mommy.make(
+            Reservation,
+            id=4,
+            rental=rental_2,
+            checkin=datetime.date(2022, 1, 2),
+            checkout=datetime.date(2022, 1, 20),
+        )
+        mommy.make(
+            Reservation,
+            id=5,
+            rental=rental_2,
+            checkin=datetime.date(2022, 1, 20),
+            checkout=datetime.date(2022, 2, 11),
+        )
 
     def test_get(self) -> None:
         response = self.client.get(self.test_url)
         self.assertEqual(response.status_code, HTTPStatus.OK)
-        self.assertTemplateUsed('rentals/list.html')
+        self.assertTemplateUsed('rentals/reservation_list.html')
         # The page should contain a table with a row for each reservation
         self.assertContains(
             response,
-            '<td>rental-1</td><td>Res-1 ID</td><td>2022-01-01</td><td>2022-01-13</td><td>-</td>',
+            '<tr><td>rental-1</td><td>Res-1</td><td>2022-01-01</td><td>2022-01-13</td><td>-</td></tr>',
             html=True,
         )
         # A row should reference a previous reservation if any
         self.assertContains(
             response,
-            '<td>rental-1</td><td>Res-2 ID</td><td>2022-01-20</td><td>2022-02-10</td><td>Res-1 ID</td>',
+            '<tr><td>rental-1</td><td>Res-2</td><td>2022-01-20</td><td>2022-02-10</td><td>Res-1</td></tr>',
             html=True,
         )
--- rentals/views.py
+++ rentals/views.py
@@ -1,3 +1,5 @@
+from django.db import models
+from django.db.models import OuterRef, QuerySet, Subquery
 from django.views.generic import ListView
 
 from rentals.models import Reservation
@@ -6,3 +8,13 @@ from rentals.models import Reservation
 class ReservationListView(ListView):
     model = Reservation
     paginate_by = 50
+    context_object_name = 'reservations'
+
+    def get_queryset(self) -> 'QuerySet[Reservation]':
+        previous_reservations = Reservation.objects.filter(
+            rental_id=OuterRef('rental_id'),
+            checkin__lt=OuterRef('checkin'),
+        ).order_by('-checkin')
+        return Reservation.objects.all().annotate(
+            previous_reservation_id=Subquery(previous_reservations.values('id')[:1], output_field=models.CharField())
+        ).select_related('rental')
